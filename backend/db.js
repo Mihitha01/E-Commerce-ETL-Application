37ebe199051e2a1abe3bd18671988439c25eac8f
Mihitha01 <mihithabandara@gmail.com>
2026-04-22 14:50:01 +0530

    fix: Update MongoDB connection string to use IP address instead of localhost

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         // Removed the deprecated options!
-        const conn = await mongoose.connect('mongodb://localhost:27017/AmazonSalesDB');
+        const conn = await mongoose.connect('mongodb://127.0.0.1:27017/AmazonSalesDB');
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error connecting to MongoDB: ${error.message}`);
